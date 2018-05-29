965579377195074738c3e990d2a1335498fb26a8
Muhammad Hafidz <halfpheez92@gmail.com>
2018-05-29 16:55:06 +1000

    modified implementation, added dynamic date range from database in table.

--- tasklist/src/webparts/spPnPjsExample/components/TimelineList/ITimelineListState.ts
+++ tasklist/src/webparts/spPnPjsExample/components/TimelineList/ITimelineListState.ts
@@ -2,4 +2,7 @@ export interface ITimelineListState {
   list?: any[];
   dates?: string[];
   start_date?: string;
+  due_date?: string;
+  date_start?: string;
+  date_range?: string[];
 }
--- tasklist/src/webparts/spPnPjsExample/components/TimelineList/TimelineList.tsx
+++ tasklist/src/webparts/spPnPjsExample/components/TimelineList/TimelineList.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { ITimelineListProps } from "../TimelineList";
 import { ITimelineListState } from "../TimelineList";
 import pnp from "sp-pnp-js";
+import { List } from "office-ui-fabric-react/lib/List";
 
 interface web {
   Title: string;
@@ -17,48 +18,34 @@ export class TimelineList extends React.Component<
     super(props);
     this.state = {
       list: [],
-      dates: [
-        "2018-05-21T14:00:00Z",
-        "2018-05-22T14:00:00Z",
-        "2018-05-23T14:00:00Z",
-        "2018-05-24T14:00:00Z",
-        "2018-05-25T14:00:00Z",
-        "2018-05-26T14:00:00Z",
-        "2018-05-27T14:00:00Z"
-      ],
-      start_date: this.props.start_date
+      date_range: []
     };
+    this._setDateArray = this._setDateArray.bind(this);
+    this._taskDetails = this._taskDetails.bind(this);
   }
 
   public render(): React.ReactElement<ITimelineListProps> {
-    console.log("test-render");
+    //console.log("test-render");
+    //console.dir(this.state.date_range);
     return (
       <div>
-        {this.state.dates.map((date, index) => {
-          // get an array of objects and iterate through each object.
-          const taskItems = this.state.list.filter(x => x.StartDate == date); // get an array of objects with filter of start date.
-          return (
-            <div>
+        <div>
+          {this.state.date_range.map((date, index) => {
+            //console.dir(date);
+            return (
+              // note: JSX cannot output objects.
+              // must convert to string.
               <table>
                 <tr>
                   <th>{date}</th>
                 </tr>
                 <tr>
-                  <th>
-                    {taskItems.map(taskItem => {
-                      return (
-                        <div>
-                          <div>Task Title: {taskItem.Title}</div>
-                          <div>Task Startdate: {taskItem.StartDate}</div>
-                        </div>
-                      );
-                    })}
-                  </th>
+                  <td>{this._taskDetails()}</td>
                 </tr>
               </table>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     );
   }
@@ -71,37 +58,65 @@ export class TimelineList extends React.Component<
       .then((items: any[]) => {
         console.log(items);
         this.setState({
-          list: items
+          list: items,
+          date_range: this._setDateArray() // set the state of date range.
         });
       });
   }
 
-  private _getTask(taskId: any): any {
-    console.log("get-tasks");
-    return this.state.list.filter(
-      (task: any) => task.AuthorId == taskId.taskId
-    );
-  }
+  private _setDateArray(): any[] {
+    console.log("set-date-array");
+    let current_date = new Date();
+    console.log(current_date);
+    let start_date = new Date(current_date.setDate(current_date.getDate() - 7));
+    console.log(start_date);
+    let end_date = new Date(current_date.setDate(current_date.getDate() + 7));
+    let newEndDate = current_date.setDate(current_date.getDate() + 7);
+    end_date = new Date(newEndDate);
+    console.log(end_date);
 
-  private _getDate(index: number): string {
-    console.log("get-date");
-    let thisDate: string = this.state.dates[index];
-    return thisDate;
-  }
-
-  private _checkTaskWithStartDate(): string {
-    console.log("check-task-startdate");
-    let start_date: string;
-    for (let i = 0; i < this.state.list.length; i++) {
-      for (let j = 0; j < this.state.dates.length; j++) {
-        if (this.state.list[i].StartDate == this.state.dates[j]) {
-          return (start_date = this._getDate(j));
-        }
-      }
+    let date_array = new Array();
+    while (start_date <= end_date) {
+      date_array.push(start_date);
+      let countDate = start_date.setDate(start_date.getDate() + 1);
+      start_date = new Date(countDate);
     }
+    console.log(date_array);
+    date_array = this._formatDates(date_array);
+    return date_array;
   }
 
+  private _formatDates(array: any[]): any[] {
+    console.log("test-format-date");
+    let temp = new Array();
+    for (let index = 0; index < array.length; index++) {
+      let dateFormat = new Date(array[index]);
+      temp.push(dateFormat.toDateString());
+    }
+    return temp;
+  }
+
+  private _taskDetails(): any {
+    console.log("task-details");
+    let temp = new Array();
+    let taskItems: any = [];
+    this.state.list.map(item => {
+      let newListDateFormat = new Date(item.StartDate);
+      //console.log(newListDateFormat.toDateString());
+      this.state.date_range.map(date => {
+        let newRangeDateFormat = new Date(date);
+        //console.log(newRangeDateFormat.toDateString());
+        taskItems = this.state.list.filter(
+          x => x.StartDate.toString() == newRangeDateFormat.toDateString()
+        );
+      });
+    });
+    console.log(taskItems);
+  }
+
+  // debug test
   private _getWebDetails(): void {
+    console.log("test-web-details");
     let html: string = "";
     const element: HTMLElement = document.getElementById("click");
     pnp.sp.web.get().then(response => {
